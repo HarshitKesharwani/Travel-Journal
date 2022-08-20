d85829703be3a0fb05d4e3ec4bf41bcb82d48619
Harshit Kesharwani <harshitkesh123@gmail.com>
2022-08-20 21:06:44 +0530

    media

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,14 +2,15 @@ import React from "react";
 export default function Navbar()
 {
     return(
-    <header className="head1">
+    <div className="head1">
        
-                
+              
+                <div col-12 col-sm-12>
                 <img  className="img1" src="./img/world.jpg"/>
                 <h4 className="text1">My Travel Journal</h4>
             
           <h1 className="head">12 Jyotirlings in India</h1>
-       
-    </header>
+       </div>
+    </div>
     )
 }
